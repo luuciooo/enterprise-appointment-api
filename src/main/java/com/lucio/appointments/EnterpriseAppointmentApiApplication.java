67e3794f20907aed575c3f472c1f01ae9d20011b
luuciooo <lucioabalos97@gmail.com>
2026-02-18 14:22:44 -0300

    feat(project): initialize spring boot project with hexagonal structure

---
+++ src/main/java/com/lucio/appointments/EnterpriseAppointmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucio.appointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppointmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAppointmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucio/enterprise_appointment_api/EnterpriseAppointmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucio.enterprise_appointment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAppointmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
